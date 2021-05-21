c3698e18c4fc77f698caaf5739b5c3f7032a866e
kenti <mopup2017@gmail.com>
2021-05-20 21:57:45 -0300

    qmp 4ta iteracion

--- src/main/java/Guardarropas.java
+++ src/main/java/Guardarropas.java
@@ -6,45 +6,59 @@ import java.util.stream.Collectors;
 
 public class Guardarropas {
 	
-	public Guardarropas(List<Prenda> prendasDisponibles, Ciudad ciudad) {
+	public Guardarropas(List<Prenda> prendasDisponibles, WeatherChecker checker) {
 		this.prendasDisponibles = prendasDisponibles;
-		this.ciudad = ciudad;
-		this.condicionesClimaticasActuales = server.getWeather(this.ciudad.getNombre());
-		}
-	
+		this.checker = checker;
+	}
+
 	private List <Prenda> prendasDisponibles;
+	private WeatherChecker checker;
+
+	public List<Atuendo> Sugerencias(){
+		return new GeneradorDeSugerencias().generarSugerencias(checker.prendasAptas(this.prendasDisponibles));  
+	}
+	
+}
+
+
+
+
+class WeatherChecker{
+	
+	
+	public WeatherChecker(Ciudad ciudad) {
+		this.ciudad= ciudad;
+		this.condicionesClimaticasActuales= server.getWeather(ciudad.getNombre());
+	}
+	
 	private Ciudad ciudad;
 	private List<Map<String, Object>> condicionesClimaticasActuales;
 	private WeatherServer server= new AccuWeatherAPI();
-
-	public List<Atuendo> Sugerencias(){
-		return new GeneradorDeSugerencias().generarSugerencias(prendasAptas(this.prendasDisponibles));  
-	}
 	
-	 public void actualizarTiempo(){
-		 LocalDateTime now = LocalDateTime.now();
-		 LocalDateTime before = (LocalDateTime) condicionesClimaticasActuales.get(0).get("DateTime");
-		 Duration duration= Duration.between(now, before);
-		 long horas= Math.abs(duration.getSeconds()) /3600;
-		 if (horas>= 12) {
-			 this.server= new AccuWeatherAPI(); 
-		 }			
-	 }
-	
-	
-	
-	private List<Prenda> prendasAptas(List<Prenda> prendas) {
+	public List<Prenda> prendasAptas(List<Prenda> prendas) {
 		return prendas.stream().filter(prenda -> esApta(prenda)).collect(Collectors.toList());
 	}
-
 
 	private Boolean esApta(Prenda prenda) {
 		return prenda.tipo.getTempMax()>= temperaturaActual();
 	}
 
+	
 	private Integer temperaturaActual() {
 		actualizarTiempo();
 		return (Integer) condicionesClimaticasActuales.get(0).get("Temperature");
 	}
+	
+	
+	public void actualizarTiempo(){
+		 LocalDateTime now = LocalDateTime.now();
+		 LocalDateTime before = (LocalDateTime) condicionesClimaticasActuales.get(0).get("DateTime");
+		 Duration duration= Duration.between(now, before);
+		 long horas= Math.abs(duration.getSeconds()) /3600;
+		 if (horas>= 12) {
+			 this.condicionesClimaticasActuales= server.getWeather(this.ciudad.getNombre()); 
+		 }			
+	 }
+	
 	
 }
