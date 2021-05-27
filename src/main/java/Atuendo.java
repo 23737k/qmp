c7ccfea6e87492742dd8d3c51c18deaf19772673
kenti <mopup2017@gmail.com>
2021-05-26 22:38:10 -0300

    agrego validaciones constructor Atuendo

--- src/main/java/Atuendo.java
+++ src/main/java/Atuendo.java
@@ -1,8 +1,12 @@
+import Excepciones.PrendaInvalidaException;
 
 public class Atuendo {
 	public Atuendo(Prenda superior, Prenda inferior, Prenda calzado) {
+		verificarCategoria(Categoria.SUPERIOR,superior);
 		this.superior = superior;
+		verificarCategoria(Categoria.INFERIOR,inferior);
 		this.inferior = inferior;
+		verificarCategoria(Categoria.CALZADO,calzado);
 		this.calzado = calzado;
 	}
 
@@ -11,4 +15,10 @@ public class Atuendo {
 	public Prenda calzado;
 
 
+	public void verificarCategoria(Categoria categoria, Prenda prenda){
+		if(prenda.getCategoria() != categoria) {
+			throw new PrendaInvalidaException ("prenda de categoria incorrecta");
+		}
+	}
+	
 }
